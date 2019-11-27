d4379011007269fda17578809df8d3d029075df9
For Ka Lau <c1941440@comsc-102722.dsa.cf.ac.uk>
2019-11-27 16:42:18 +0000

    finished

--- simulation.py
+++ simulation.py
@@ -1,64 +1,153 @@
-import queue
 import sqlite3
-import multiprocessing
-import time
+from multiprocessing import Process
 import re
 
-class simulation_system(object):
-    def init_system(self):
-        self.system = queue.Queue()
-        return print("** SYSTEM INITIALISED **")
+class Queue(object):
+    def __init__(self):
+        self.p1 = []
+        self.p2 = []
 
-    def system_acquireData(self):
+    def enqueue(self, item):
+        self.p1.append(item)
+
+    def dequeue(self):
+        self.front()
+        return self.p2.pop()
+
+    def front(self):
+        if not self.p2:
+            while self.p1:
+                    self.p2.append(self.p1.pop())
+        return self.p2[-1]
+
+    def isEmpty(self):
+        return not self.p1 and not self.p2
+
+
+class simulation_system(Queue):
+
+    def init_system(self):
+        self.q = Queue()
         connection = sqlite3.connect('Simulation_data.db')
-        cur = connection.cursor() 
-        cur.execute("SELECT * FROM dataset")  
-        tasks = cur.fetchall()
-        pattern = re.compile(r"((?=.*[\d])(?=.*[a-z])(?=.*[A-Z])|(?=.*[\d])(?=.*[a-zA-Z])(?=.*[@''#*&-])|(?=.*[a-z])(?=.*[A-Z])(?=.*[@''#*&-])).*")
-        for i in tasks:
-            print(f"** [{time.perf_counter()}] : Task [{i[0]}] with duration [{i[2]}] enters the system.")
-        for i in tasks:
-            if pattern.match(i[0]):
-                print(f"** Task [{i[0]}] accepted.")
-                self.system.put(i)
-            else:
-                print(f"** Task [{i[0]}] unfeasible and discarded.")
+        cur = connection.cursor()
+        cur.execute("SELECT * FROM dataset ORDER BY arrival ASC")
+        tasksList = cur.fetchall()
+        print(tasksList)
+        for i in tasksList:
+            self.q.enqueue(i)
+        self.clock = 0
+        self.onHold = 0
+        self.processor1 = []
+        self.processor2 = []
+        self.processor3 = []
         connection.commit()
         connection.close()
-    
+        return print("** SYSTEM INITIALISED **")
+
+    def enter_system(self):
+        if self.q.isEmpty() is False:
+            EnteringItem = self.q.front()
+            self.clock = EnteringItem[1]
+            print(f"** [{self.clock}] : Task [{EnteringItem[0]}] with duration [{EnteringItem[2]}] enters the system.")
+            pattern = re.compile(r"((?=.*[\d])(?=.*[a-z])(?=.*[A-Z])|(?=.*[\d])(?=.*[a-zA-Z])(?=.*[@#*&_-])|(?=.*[a-z])(?=.*[A-Z])(?=.*[@#*&_-])).*")
+            if pattern.match(EnteringItem[0]):
+                print(f"** Task [{EnteringItem[0]}] accepted.")
+                self.assign_processor()
+            else:
+                print(f"** Task [{EnteringItem[0]}] unfeasible and discarded.")
+                self.q.dequeue()
+                self.enter_system()
+        else:
+            self.complete_tasks()
+
     def assign_processor(self):
-        onHold = []
-        p1 = multiprocessing.Process(target=self.system.get)
-        p2 = multiprocessing.Process(target=self.system.get)
-        p3 = multiprocessing.Process(target=self.system.get)
-        p1.start()
-        p2.start()
-        p3.start()
+        EnteringItem = self.q.front()
+        if len(self.processor1) == 0:
+                print(f"** [{self.clock}] : Task [{EnteringItem[0]}] assigned to processor [1]")
+                self.processor1.append(self.q.front())
+                self.q.dequeue()
+                self.complete_tasks()
 
-        while not self.system.empty():
-            onHold.append(self.system.get())
-        print(onHold)
+        elif len(self.processor2) == 0:
+                print(f"** [{self.clock}] : Task [{EnteringItem[0]}] assigned to processor [2]")
+                self.processor2.append(self.q.front())
+                self.q.dequeue()
+                self.complete_tasks2()
 
-    # Then, the task needs to be assigned to a processor. If a processor is
-# available, then the task is assigned to it, the processor is busy for the whole
-# duration of the task and it becomes available when it ends. Otherwise,
-# the task must be put on hold and assigned to the first available processor
-# according to a FIFO strategy.
-# When a task is put on hold the message displayed is:
-# ** Task [TASK ID] on hold.
-# On the other hand, when a task is assigned to a processor the following
-# message is displayed:
-# ** [CLOCK] : Task [TASK ID] assigned to processor [PROCESSOR #].
-# where [PROCESSOR #] is the processor number, i.e., either 1, 2 or 3.
-# When a task is completed, the message displayed is:
-# 3
-# ** [CLOCK] : Task [TASK ID] completed.
- 
-    def system_end(self):
-        return print( f"** [{time.perf_counter()}] : SIMULATION COMPLETED. **")
+        elif len(self.processor3) == 0:
+                print(f"** [{self.clock}] : Task [{EnteringItem[0]}] assigned to processor [3]")
+                self.processor3.append(self.q.front())
+                self.q.dequeue()
+                self.complete_tasks3()
+
+        elif self.onHold == 0:
+            print(f'** Task [{EnteringItem[0]}] on hold')
+            self.onHold = 1
+            self.complete_tasks()
+
+    def complete_tasks(self):
+        try:
+            EnteringItem = self.q.front()
+            AssignedItem1 = self.processor1[0]
+
+            if self.onHold == 1:
+                print(f"** [{self.clock}] : Task [{AssignedItem1[0]}] completed.")
+                self.processor1.pop()
+                self.onHold = 0
+                self.assign_processor()
+
+            elif EnteringItem[1] > AssignedItem1[1] + AssignedItem1[2]:
+                print(f"** [{self.clock}] : Task [{AssignedItem1[0]}] completed.")
+                self.processor1.pop()
+                self.enter_system()
+
+            else:
+                self.enter_system()
+        except:
+            self.complete_tasks2()
+
+    def complete_tasks2(self):
+        try:
+            EnteringItem = self.q.front()
+            AssignedItem2 = self.processor2[0]
+            if self.onHold == 1:
+                print(f"** [{self.clock}] : Task [{AssignedItem2[0]}] completed.")
+                self.processor2.pop()
+                self.onHold = 0
+                self.assign_processor()
+
+            elif EnteringItem[1] > AssignedItem2[1] + AssignedItem2[2]:
+                print(f"** [{self.clock}] : Task [{AssignedItem2[0]}] completed.")
+                self.processor2.pop()
+                self.enter_system()
+            else:
+                self.enter_system()
+        except:
+            self.complete_tasks3()
+
+    def complete_tasks3(self):
+        try:
+            EnteringItem = self.q.front()
+            AssignedItem3 = self.processor3[0]
+            if self.onHold == 1:
+                print(f"** [{self.clock}] : Task [{AssignedItem3[0]}] completed.")
+                self.processor3.pop()
+                self.onHold = 0
+                self.assign_processor()
+
+            elif EnteringItem[1] > AssignedItem3[1] + AssignedItem3[2]:
+                print(f"** [{self.clock}] : Task [{AssignedItem3[0]}] completed.")
+                self.processor3.pop()
+                self.enter_system()
+
+            else:
+                self.enter_system()
+        except:
+            print(f"[{self.clock}] : SIMULATION COMPLETED.")
+
+
+
 
 Simulation = simulation_system()
 InitSystem = Simulation.init_system()
-EnterSystem = Simulation.system_acquireData()
-AssignProcessors = Simulation.assign_processor()
-SystemEnd =  Simulation.system_end()
+StartSystem = Simulation.enter_system()
--- simulation_data.py
+++ simulation_data.py
@@ -7,7 +7,7 @@ def id_generator():
     length=6
     id_num += string.digits
     id_num += string.ascii_letters
-    id_num += "@''#*&-"
+    id_num += "@#*&_-"
     return ''.join(random.choice(id_num) for _ in range(length))
 
 
@@ -17,17 +17,21 @@ def insert_database():
     cur.execute("""CREATE TABLE IF NOT EXISTS dataset (
     id TEXT PRIMARY KEY UNIQUE NOT NULL,
     arrival REAL NOT NULL,
-    duration REAL NOT NULL
+    duration INTEGER NOT NULL
     )""")
-    for _ in range(100):
-        ID = id_generator()
-        Arrival = random.uniform(1,100)
-        Duration = round(random.expovariate(1), 1)
-        cur.execute("INSERT INTO dataset(id, arrival, duration) VALUES(?,?,?)", 
-                    (ID, Arrival, Duration))
+    print(len(cur.fetchall()))
+    cur.execute("SELECT * FROM dataset ORDER BY arrival ASC")
+    if len(cur.fetchall()) < 100:
+        for _ in range(100):
+            ID = id_generator()
+            Arrival = random.uniform(1,100)
+            Duration = round(random.expovariate(1))+1
+            cur.execute("INSERT INTO dataset(id, arrival, duration) VALUES(?,?,?)", (ID, Arrival, Duration))
+    cur.execute("SELECT * FROM dataset ORDER BY arrival ASC")
+    print(cur.fetchall())
     connection.commit()
     connection.close()
-    
+
 insert_database()
 
 
